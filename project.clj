a22a6840d5f5b2b3011b079a445e8e252c27dd9d
Eamonn O'Brien-Strain <eob@well.com>
2014-05-25 18:48:26 -0700

    As generated by cljs-kickoff template.

---
+++ project.clj
@@ -0,0 +1,21 @@
+(defproject fgosite "0.1.0-SNAPSHOT"
+  :description "FIXME: write this!"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-2156"]
+                 [ring "1.2.1"]]
+  :plugins [[lein-cljsbuild "1.0.2"]
+            [lein-ring "0.8.10"]]
+  :hooks [leiningen.cljsbuild]
+  :source-paths ["src/clj"]
+  :cljsbuild { 
+    :builds {
+      :main {
+        :source-paths ["src/cljs"]
+        :compiler {:output-to "resources/public/js/cljs.js"
+                   :optimizations :simple
+                   :pretty-print true}
+        :jar true}}}
+  :main fgosite.server
+  :ring {:handler fgosite.server/app})
+
