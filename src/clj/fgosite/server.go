f12773741f414f8bbd00bb6e44e88f5b4a9d6562
Eamonn O'Brien-Strain <eob@well.com>
2014-05-26 19:14:28 -0700

    Remove unnecessary liberator layer

--- src/clj/fgosite/server.go
+++ src/clj/fgosite/server.go
@@ -4,17 +4,12 @@ import (
 	"ring/adapter/jetty"
 	"compojure/route"
 	compojure "compojure/core"
-	rest "liberator/core"
 )
 
-rest.defresource(helloWorld,
-	AVAILABLE_MEDIA_TYPES, ["text/plain"],
-	HANDLE_OK,"Hello, world! -- 2"
-)
 
 compojure.defroutes(app,
+	compojure.GET("/fgo/:id", [id], str("id='", id, "'")),
 	compojure.GET("/",      [], io.resource("public/index.html")),
-	compojure.GET("/hello", [], helloWorld),
 	route.resources("/"),
 	route.notFound("<h1>Page not found</h1>")
 )
