2daa76aaf835120e08f9d6fca573919bb37dfff5
Eamonn O'Brien-Strain <eob@well.com>
2014-06-13 22:16:31 -0700

    Update funcgo version used.

--- project.clj
+++ project.clj
@@ -1,16 +1,16 @@
 (defproject fgosite "0.1.1"
-  :description "FIXME: write this!"
-  :url "http://example.com/FIXME"
+  :description "Web site for Funcgo, written in Funcgo"
+  :url "https://github.com/eobrain/funcgo"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.eamonn.funcgo/funcgo-lein-plugin "0.2.4-SNAPSHOT"]
-                 [org.eamonn.funcgo/fgolib "0.2.2-SNAPSHOT"]
+                 [org.eamonn.funcgo/funcgo-lein-plugin "0.2.5"]
+                 [org.eamonn.funcgo/fgolib "0.2.3"]
                  [org.clojure/clojurescript "0.0-2156"]
                  [ring "1.2.1"]
                  [compojure "1.1.8"]
                  [hiccups "0.3.0"]
                  [midje "1.5.1" :scope "test"]]
   :test-paths ["resources/public/tour_code"]
-  :plugins [[org.eamonn.funcgo/funcgo-lein-plugin "0.2.4-SNAPSHOT"]
+  :plugins [[org.eamonn.funcgo/funcgo-lein-plugin "0.2.5"]
             [lein-cljsbuild "1.0.2"]
             [lein-ring "0.8.10"]
             [lein-midje "3.1.1"]]
