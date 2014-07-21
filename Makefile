d770f836ca4aa5b2e31b4f5fa148a088e6a3f5df
Eamonn O'Brien-Strain <eob@well.com>
2014-07-20 22:54:45 -0700

    Version 0.1.5 -- App Engine deployment

---
+++ Makefile
@@ -0,0 +1,25 @@
+# For deployment
+
+VERSION=0.1.4-SNAPSHOT
+
+WAR=target/fgosite-$(VERSION)-standalone.war
+
+
+appengine: $(WAR)
+	: Deploy to App Engine
+	unzip -q $(WAR) -d /tmp/exploded$$$$; appcfg.sh --oauth2 update /tmp/exploded$$$$
+
+appengine-local: $(WAR)
+	: Run on App Engine local development server
+	unzip -q $(WAR) -d /tmp/exploded$$$$; dev_appserver.sh /tmp/exploded$$$$
+
+heroku: $(WAR)
+	: Deploy on Heroku
+	heroku deploy:war --war $(WAR)
+
+$(WAR):
+	lein do  clean,  fgoc --force,  cljsbuild once,  ring uberwar
+
+local:
+	: Run locally
+	lein do  clean,  fgoc --force,  cljsbuild once,  ring server-headless
--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject fgosite "0.1.4"
+(defproject fgosite "0.1.5"
   :description "Web site for Funcgo, written in Funcgo"
   :url "https://github.com/eobrain/funcgo"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.303.0-886421-alpha"]
-                 [org.eamonn.funcgo/funcgo-lein-plugin "0.3.0"]
+                 [org.eamonn.funcgo/funcgo-lein-plugin "0.4.1"]
                  [org.eamonn.funcgo/fgolib "0.2.5"]
                  [org.clojure/clojurescript "0.0-2156"]
                  [ring "1.2.1"]
@@ -13,7 +13,7 @@
                  [midje "1.6.3" :scope "test"]]
   :min-lein-version "2.0.0"
   :test-paths ["resources/public/tour_code"]
-  :plugins [[org.eamonn.funcgo/funcgo-lein-plugin "0.3.0"]
+  :plugins [[org.eamonn.funcgo/funcgo-lein-plugin "0.4.1"]
             [lein-cljsbuild "1.0.2"]
             [lein-ring "0.8.10"]
             [lein-midje "3.1.1"]]
